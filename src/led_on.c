f11db0d21d1119932fa047294c0f938942e6dc2c
theeacx <licxandruteodora@gmail.com>
2026-05-24 22:45:28 +0300

    LED on

---
+++ src/led_on.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>
+
+#define GPIO_LED 529
+
+static int __init led_hbd_init(void)
+{
+    int ret;
+
+    pr_info("led_hbd: init - LED permanently ON\n");
+
+    ret = gpio_request(GPIO_LED, "led_gpio");
+    if (ret) {
+        pr_err("led_hbd: gpio_request failed\n");
+        return ret;
+    }
+
+    gpio_direction_output(GPIO_LED, 1);
+
+    pr_info("led_hbd: LED is ON\n");
+    return 0;
+}
+
+static void __exit led_hbd_exit(void)
+{
+    pr_info("led_hbd: exit\n");
+    gpio_set_value(GPIO_LED, 0);
+    gpio_free(GPIO_LED);
+}
+
+module_init(led_hbd_init);
+module_exit(led_hbd_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Thea");
+MODULE_DESCRIPTION("LED permanently on");
