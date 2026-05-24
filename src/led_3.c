a85c9e8838a11951e02edba7e802f8074ac50e06
theeacx <licxandruteodora@gmail.com>
2026-05-24 17:20:01 +0300

    LED blink 3

--- src/led_3.c
+++ src/led_3.c
@@ -1,14 +1,25 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
+#include <linux/timer.h>
 
 #define GPIO_LED 529
+
+static struct timer_list blink_timer;
+static int led_state = 0;
+
+static void blink_callback(struct timer_list *t)
+{
+    led_state = !led_state;
+    gpio_set_value(GPIO_LED, led_state);
+    mod_timer(&blink_timer, jiffies + 3 * HZ);
+}
 
 static int __init led_hbd_init(void)
 {
     int ret;
 
-    pr_info("led_hbd: init - LED ON mode\n");
+    pr_info("led_hbd: init - blink every 3 seconds\n");
 
     ret = gpio_request(GPIO_LED, "led_gpio");
     if (ret) {
@@ -16,20 +27,24 @@ static int __init led_hbd_init(void)
         return ret;
     }
 
-    ret = gpio_direction_output(GPIO_LED, 1);
+    ret = gpio_direction_output(GPIO_LED, 0);
     if (ret) {
         pr_err("led_hbd: gpio_direction_output failed\n");
         gpio_free(GPIO_LED);
         return ret;
     }
 
-    pr_info("led_hbd: LED is ON\n");
+    timer_setup(&blink_timer, blink_callback, 0);
+    mod_timer(&blink_timer, jiffies + 3 * HZ);
+
+    pr_info("led_hbd: blinking started\n");
     return 0;
 }
 
 static void __exit led_hbd_exit(void)
 {
-    pr_info("led_hbd: exit - turning LED off\n");
+    pr_info("led_hbd: exit\n");
+    del_timer_sync(&blink_timer);
     gpio_set_value(GPIO_LED, 0);
     gpio_free(GPIO_LED);
 }
@@ -39,4 +54,4 @@ module_exit(led_hbd_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Thea");
-MODULE_DESCRIPTION("Simple LED always-on module");
+MODULE_DESCRIPTION("LED blink every 3 seconds");
